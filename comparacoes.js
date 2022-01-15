fa3b45710e1dd23930f99ef9b9774023efc96123
Charlim <carloslanderdahl27@gmail.com>
2022-01-14 21:36:43 -0300

    Adicionando o arquivo (comparacoes)

---
+++ comparacoes.js
@@ -0,0 +1,18 @@
+// == (comparacão implicita)
+
+const numero = 5
+const text = "5"
+
+console.log(numero === text)
+
+//typeoff
+console.log(typeof numero)
+console.log(typeof text)
+
+// == só compara valor
+// === compara o valor e o tipo de dado
+
+//conversao de modo explicito
+
+Number()
+String()
