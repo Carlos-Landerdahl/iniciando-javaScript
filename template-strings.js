2223d017d961596f0bf90c454308e1fc95fea2ef
Charlim <carloslanderdahl27@gmail.com>
2022-01-14 22:02:40 -0300

    Template-String (Muito interressante)

---
+++ template-strings.js
@@ -0,0 +1,13 @@
+const nome = "Carlos"
+const idade = 2021-2000
+const cidadeDeNascimento = "UDI"
+
+
+
+//const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}.`
+
+console.log(apresentacao)
+
+
